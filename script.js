0f51aed8090cdc9547b392a8d71ebf235fbe7461
layavaca <56559559+layavaca@users.noreply.github.com>
2023-04-13 09:01:17 -0500

    js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 console.log('Hello!');
+
+var i = 0;
+var elemento = document.getElementById('titulo1');
+elemento.addEventListener('click', function () {
+  // alert("click en titulo");
+  //elemento.innerHTML = i;
+  crearParrfo();
+});
+
+var elemento = document.getElementById('addlink');
+elemento.addEventListener('click', function () {
+  crearParrfo();
+});
+elemento.addEventListener('mouseover', function () {
+  i = i + 1;
+  console.log(i);
+});
+
+var lista = document.getElementsByTagName('a');
+console.log('lista');
+
+//var cabecera = document.getElementById("cabecera");
+//var lista = document.getElementById("li");
+//console.log(lista)
+
+//var list = document.querySelectorAll("#menu li")
+//console.log(list)
+
+//var lista2 = document.querySelectorAll()
+
+
+//)
+//como crear un elemento desde js
+//function crearParrfo(){
+
+function addLink(){
+  console.log("add link 1")
+  var link = document.createElement("li")
+  var ul =document.querySelector("aside ul")
+  ul.appendChild(link)
+}
